2db65defaf16534d307e43a242be4fd6b9c9d658
Crispin Wellington <retrogradeorbit@gmail.com>
2020-07-06 23:46:56 +0800

    initial commit

---
+++ src/clj/nativegl/core.clj
@@ -0,0 +1,7 @@
+(ns nativegl.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/nativegl/core_test.clj
@@ -0,0 +1,7 @@
+(ns nativegl.core-test
+  (:require [clojure.test :refer :all]
+            [nativegl.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
