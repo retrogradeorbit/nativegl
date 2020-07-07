a01ef9d8b3ea2c41974bf6e90e826b5c9f51634a
Crispin Wellington <retrogradeorbit@gmail.com>
2020-07-07 11:40:37 +0800

    echo command

--- .appveyor/compile.bat
+++ .appveyor/compile.bat
@@ -18,6 +18,7 @@ mkdir target
 mkdir target/jni
 call javac -h target/jni src/c/NativeGL.java
 
+echo cl.exe /I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" /I"target\jni" /MD /LD /Fenativegl.dll src/c/NativeGL.c
 call cl.exe /I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" /I"target\jni" /MD /LD /Fenativegl.dll src/c/NativeGL.c
 
 call lein do clean, uberjar
