e5d1f32c3d7073eeec0a601521920838fcf71863
Crispin Wellington <retrogradeorbit@gmail.com>
2020-07-09 21:47:55 +0800

    SDL2 calls

--- src/c/NativeGL.c
+++ src/c/NativeGL.c
@@ -1,6 +1,21 @@
 #include "NativeGL.h"
+#include "SDL.h"
 
 JNIEXPORT jint JNICALL Java_NativeGL_is_1a_1tty(JNIEnv *env, jclass cls)
 {
   return 1;
+}
+
+JNIEXPORT jint JNICALL Java_NativeGL_init(JNIEnv *env, jclass cls)
+{
+   if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0) {
+     SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+     return 1;
+   }
+   return 0;
+}
+
+JNIEXPORT void JNICALL Java_NativeGL_quit(JNIEnv *env, jclass cls)
+{
+  SDL_Quit();
 }
--- src/c/NativeGL.java
+++ src/c/NativeGL.java
@@ -1,4 +1,8 @@
 public class NativeGL {
     // terminal tty
     public static native int is_a_tty();
+
+    public static native int init();
+    public static native void quit();
+
 }
